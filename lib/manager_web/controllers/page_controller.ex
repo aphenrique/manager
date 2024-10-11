783b6f159643878e407b0d916d0e1854ed073fa2
aphenrique <ap_henrique@hotmail.com>
2024-10-10 22:04:50 -0300

    Cria redirecionamento para a página de login ou contas

--- lib/manager_web/controllers/page_controller.ex
+++ lib/manager_web/controllers/page_controller.ex
@@ -4,6 +4,13 @@ defmodule ManagerWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    if conn.assigns.current_user do
+      redirect(conn, to: ~p"/transactions")
+    else
+      redirect(conn, to: ~p"/users/log_in")
+    end
+
+    # render(conn, :home, layout: false)
   end
 end
